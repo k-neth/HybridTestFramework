4ee1bd8910d4e3f02280017688c1a5e9f6d8f1fe
k-neth <kneth46@gmail.com>
2024-09-18 02:53:36 +0300

    bat

--- run.bat
+++ run.bat
@@ -1,2 +1,2 @@
 @REM pytest -s -v -m "sanity" --html=./Reports/report.html testCases/ --browser chrome
-pytest  -v -s testCases\test_jumia.py --browser=chrome --html=./Reports/myreport.html --log-cli-level=debug
+pytest  -v -s testCases\test_jumia.py --browser=edge  --html=./Reports/myreport.html --log-cli-level=INFO 
